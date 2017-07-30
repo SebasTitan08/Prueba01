306c2cd4a45310e11ff1f72380d29730f0da4622
jhoan <jhoan@DESKTOP-HVR5GQC>
2017-07-30 13:22:22 -0500

    gfh

---
+++ prueba/src/main/java/prueba/prueba/App.java
@@ -0,0 +1,17 @@
+package prueba.prueba;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        BeanFactory ctx = new ClassPathXmlApplicationContext();
+    }
+}
